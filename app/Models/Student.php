00c78605a81edcf759ca8f1ed48458c365536f9a
SalNk <salemnk02@gmail.com>
2023-08-06 01:41:39 +0200

    Création des migrations

---
+++ app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'first_name',
+        'class',
+        'status',
+        'birth',
+        'phone_number',
+    ];
+}
---
+++ database/migrations/2023_08_01_103408_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('first_name');
+            $table->string('class');
+            $table->string('status');
+            $table->date('birth');
+            $table->string('phone_number');
+            $table->foreignId('class_id')->constrained('classes');
+            $table->foreignId('section_id')->constrained('sections');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
